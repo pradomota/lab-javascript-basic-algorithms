de2939f034e685b80565291c0d31d6213f3ad28b
Julio García Pérez <julio.garciaperez@telefonica.com>
2021-03-27 09:53:14 +0100

    fix: change typo in main file

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,6 +1,6 @@
 // Names and Input
 var hacker1 = "Carlos";
-console.log("The driver's name is " + hacker1);
+console.log("The Driver's name is " + hacker1);
 
 var hacker2 = prompt("What is the navigator's name?", "");
 console.log("The navigator's name is " + hacker2);
